8753fd6724a6332694d31d3dfd5e55cd34289ca0
Marcel Goethals <marcel.goethals@gmail.com>
2017-11-24 15:50:46 +0100

    Docker

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 sed -i "s^__PORT__^$PORT^g" /app/config.json
 sed -i "s^__DB_STORAGE__^$DB_STORAGE^g" /app/config.json
-sed -i "s^__MAIL_APIKEY__^$DB_STORAGE^g" /app/config.json
-sed -i "s^__MAIL_DOMAIN__^$DB_STORAGE^g" /app/config.json
+sed -i "s^__MAIL_APIKEY__^$MAIL_APIKEY^g" /app/config.json
+sed -i "s^__MAIL_DOMAIN__^$MAIL_DOMAIN^g" /app/config.json
 
 
 cd /app
