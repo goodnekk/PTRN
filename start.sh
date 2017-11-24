10ddf9a1c86a1a68b6bc7e08d28d7ba7aa0be4dd
Marcel Goethals <marcel.goethals@gmail.com>
2017-11-24 10:03:00 +0100

    Docker

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 
 sed -i "s^__PORT__^$PORT^g" /app/config.json
 sed -i "s^__DB_STORAGE__^$DB_STORAGE^g" /app/config.json
+sed -i "s^__MAIL_APIKEY__^$DB_STORAGE^g" /app/config.json
+sed -i "s^__MAIL_DOMAIN__^$DB_STORAGE^g" /app/config.json
+
 
 cd /app
 npm start
